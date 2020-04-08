d0cb51034c1652665839f74d4c5d3df638ff6b4a
Luanr <cbluan@gmail.com>
2020-04-08 16:45:41 -0300

    mongodb settings

--- flexnews/settings.py
+++ flexnews/settings.py
@@ -74,10 +74,12 @@ WSGI_APPLICATION = 'flexnews.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default' : {
+      'ENGINE' : 'django_mongodb_engine',
+      'NAME' : 'flex-challenge',
+      'USER' : 'root',
+      'PASSWORD' : 'example'
+   }
 }
 
 
