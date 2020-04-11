ee7fbf5dac0944c52f30b71fdf7f88d03af464e5
Luanr <cbluan@gmail.com>
2020-04-11 12:26:32 -0300

    added models

--- news/models.py
+++ news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class News(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    date = models.DateField(auto_now_add=True)
+    title = models.CharField(max_length=60)
+    text = models.TextField()
