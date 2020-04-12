663ffc818726a325e35ea87680334e9b46078e8e
Luanr <cbluan@gmail.com>
2020-04-12 18:33:13 -0300

    added list views

--- news/views.py
+++ news/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from news.models import News
+from news.serializers import NewsSerializer
 
-# Create your views here.
+@csrf_exempt
+def news_list(request):
+    if request.method == 'GET':
+        news = News.objects.all()
+        serializer = NewsSerializer(news, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+@csrf_exempt
+def news_datail(request, pk):
+    try:
+        news = News.objects.get(pk=pk)
+    except News.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        news = News.objects.all()
+        serializer = NewsSerializer(news, many=True)
+        return JsonResponse(serializer.data, safe=False)
